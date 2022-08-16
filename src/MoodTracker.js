226300bcdd444373b394aa0342a578eddea1937f
fakihap <fakih.anugerah@gmail.com>
2022-08-16 15:26:45 +0700

    fix: cant access score when localStorage is empty

--- src/MoodTracker.js
+++ src/MoodTracker.js
@@ -12,6 +12,7 @@ const result_arr = ['Happy' , 'Neutral' , 'Sad']
 
 // for user history
 var history_score = JSON.parse(localStorage.getItem("history_array"));
+if (history_score == null) history_score = [] // catch if empty
 const history_prompt = document.querySelector(".history-prompt")
 const last_result_bar = document.getElementById("last-result-bar")
 const all_result_bar = document.getElementById("all-result-bar")
